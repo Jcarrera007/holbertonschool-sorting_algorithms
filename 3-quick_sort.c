a7cf97b455a8b194413b772da991697691f08438
Jcarrera007 <carrerasproductions@gmail.com>
2025-04-09 15:23:51 -0400

    fix the issue

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -26,24 +26,24 @@ void swap(int *a, int *b)
  */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-	int pivot = array[high];
-	int i = low, j;
+    int pivot = array[high];
+    int i = low, j;
 
-	for (j = low; j < high; j++)
-	{
-		if (array[j] < pivot)
-		{
-			swap(&array[i], &array[j]);
-			if (i != j)
-				print_array(array, size);
-			i++;
-		}
-	}
-	swap(&array[i], &array[high]);
-	if (i != high)
-		print_array(array, size);
+    for (j = low; j < high; j++)
+    {
+        if (array[j] < pivot)
+        {
+            swap(&array[i], &array[j]);
+            if (array[i] != array[j])
+                print_array(array, size);
+            i++;
+        }
+    }
+    swap(&array[i], &array[high]);
+    if (array[i] != array[high])
+        print_array(array, size);
 
-	return i;
+    return i;
 }
 
 /**
